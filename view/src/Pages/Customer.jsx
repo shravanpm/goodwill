440bfbc16ea8b8092394db8e0d38349deae84bff
shravanpm <shravan.pm@yahoo.com>
2022-10-19 10:51:55 +0530

    customer info

--- view/src/Pages/Customer.jsx
+++ view/src/Pages/Customer.jsx
@@ -1,9 +1,25 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getCustomerInfo } from "../Redux/CustomerReducer/action";
 
 export const Customer = () => {
   const { id } = useParams();
+  const dispatch = useDispatch();
+  const [test, setTest] = useState(false);
+  //token
+  const token = useSelector((state) => state.authReducer.token);
 
-  return <Box>Customer</Box>;
+  useEffect(() => {
+    dispatch(getCustomerInfo({ id, token }));
+  }, [test]);
+
+  return (
+    <Box>
+      <Box>
+        <Button>View Customer Info</Button>
+      </Box>
+    </Box>
+  );
 };
--- view/src/Redux/CustomerReducer/action.js
+++ view/src/Redux/CustomerReducer/action.js
@@ -1,3 +1,28 @@
 import axios from "axios";
 import { baseUrl } from "../../util";
 import * as types from "./actionTypes";
+
+export const getCustomerInfo = (payload) => (dispatch) => {
+  dispatch({ type: types.GET_CUSTOMER_INFO_REQUEST });
+
+  return axios({
+    url: baseUrl + "shopcustomer/" + payload.id,
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + payload.token,
+    },
+  })
+    .then((r) => {
+      dispatch({ type: types.GET_CUSTOMER_INFO_SUCCESS, payload: r.data });
+      console.log(r.data);
+      console.log("hello");
+    })
+    .catch((e) => {
+      console.log(e.response.data);
+      if (e.response.data.message == "data not found") {
+        dispatch({ type: types.GET_CUSTOMER_INFO_SUCCESS, payload: {} });
+      } else {
+        dispatch({ type: types.GET_CUSTOMER_INFO_FAILURE });
+      }
+    });
+};
